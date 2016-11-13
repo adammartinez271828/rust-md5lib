312b39f4ac32e7786cb84f418b9f3a9c14309dee
Adam Martinez <adam.paul.martinez@gmail.com>
2016-11-12 19:31:56 -0500

    Commit changes from cargo fmt
    I don't want this to pollute my workspace as a "change" forever.

--- src/lib.rs
+++ src/lib.rs
@@ -22,13 +22,13 @@ pub extern "C" fn rol(v: u32, amt: i16) -> u32 {
 }
 
 // pub extern "C" fn count_substrings(value: *const c_char, substr: *const c_char) -> i32 {
-    // let c_value = unsafe { CString::new(value, false) };
-    // let c_substr = unsafe { CString::new(substr, false) };
-    // match c_value.as_str() {
-    //     Some(value) => match c_substr.as_str() {
-    //         Some(substr) => value.match_indices(substr).count() as i32,
-    //         None => -1,
-    //     },
-    //     None => -1,
-    // }
+// let c_value = unsafe { CString::new(value, false) };
+// let c_substr = unsafe { CString::new(substr, false) };
+// match c_value.as_str() {
+//     Some(value) => match c_substr.as_str() {
+//         Some(substr) => value.match_indices(substr).count() as i32,
+//         None => -1,
+//     },
+//     None => -1,
+// }
 // }
